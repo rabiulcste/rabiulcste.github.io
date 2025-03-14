9034a9505069b9760c88744e6e404476ad5b14d4
rabiul <awalrabiul6@gmail.com>
2025-03-14 03:20:53 -0400

    fixes

--- js/rabiul-blog.js
+++ js/rabiul-blog.js
@@ -41,44 +41,25 @@ $(document).ready(function() {
     $('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
 });
 
-// Navigation Scripts to Show Header on Scroll-Up
-jQuery(document).ready(function($) {
-    var MQL = 1170;
-
-    //primary navigation slide-in effect
-    if ($(window).width() > MQL) {
-        var headerHeight = $('.navbar-custom').height(),
-            bannerHeight  = $('.intro-header .container').height();     
-        $(window).on('scroll', {
-                previousTop: 0
-            },
-            function() {
-                var currentTop = $(window).scrollTop(),
-                    $catalog = $('.side-catalog');
-
-                //check if user is scrolling up by mouse or keyborad
-                if (currentTop < this.previousTop) {
-                    //if scrolling up...
-                    if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
-                        $('.navbar-custom').addClass('is-visible');
-                    } else {
-                        $('.navbar-custom').removeClass('is-visible is-fixed');
-                    }
-                } else {
-                    //if scrolling down...
-                    $('.navbar-custom').removeClass('is-visible');
-                    if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
-                }
-                this.previousTop = currentTop;
-
-
-                //adjust the appearance of side-catalog
-                $catalog.show()
-                if (currentTop > (bannerHeight + 41)) {
-                    $catalog.addClass('fixed')
-                } else {
-                    $catalog.removeClass('fixed')
-                }
-            });
-    }
+// Simple Navigation Scripts to Show/Hide Header on Scroll
+$(document).ready(function() {
+    var lastScrollTop = 0;
+    var navbar = $('.navbar');
+    
+    $(window).scroll(function() {
+        var currentScroll = $(this).scrollTop();
+        
+        if (currentScroll > lastScrollTop && currentScroll > 50) {
+            // Scrolling down & past the navbar
+            navbar.addClass('is-fixed').removeClass('is-visible');
+        } else {
+            // Scrolling up
+            if (currentScroll > 0) {
+                navbar.addClass('is-fixed is-visible');
+            } else {
+                navbar.removeClass('is-fixed is-visible');
+            }
+        }
+        lastScrollTop = currentScroll;
+    });
 });
